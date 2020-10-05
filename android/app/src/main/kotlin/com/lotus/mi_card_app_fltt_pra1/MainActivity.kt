cba3f86ba1cbc972bfae99761a1b5cee73efa575
selpeapi <selpeapi@gmail.com>
2020-10-05 23:49:41 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/lotus/mi_card_app_fltt_pra1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lotus.mi_card_app_fltt_pra1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
+}
