8482c88b019bdd123572cde84833edb6a2b0b139
selpeapi <selpeapi@gmail.com>
2020-10-07 00:24:29 +0900

    SafeArea 관련 추가

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
+        //SafeArea 사용 시, 상태바 등 불필요 UI 침범 X
         body: SafeArea(
           child: Container(
             height: 100.0,
             width: 100.0,
-            //마진, 위젯 외부의 여백
-            //전부
-//            margin: EdgeInsets.all(20.0),
-            //상하단, 좌우 대칭
-//            margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
-            //왼쪽, 위, 오른쪽, 아래, 각각의 값
-//            margin: EdgeInsets.fromLTRB(20, 02, 03, 23),
-            //한쪽만 값 지정
-            margin: EdgeInsets.only(left: 20),
-            //패딩, 위젯 내부의 여백
-            //전부
-            padding: EdgeInsets.all(20),
-            color: Colors.white70,
+            color: Colors.amber,
             //child, 한가지의 위젯만 가짐(예) Text, Image 배치 X)
             child: Text("있지, 예지"),
           ),
