68bf6196d49777082a10ef9bf92dec459c9cac60
selpeapi <selpeapi@gmail.com>
2020-10-09 23:55:27 +0900

    명함 앱 만들기 Step. 3.1 (전화번호 추가) +아이콘

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,40 @@ class MyApp extends StatelessWidget {
                 letterSpacing: 13,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25,
+              ),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Container(
+                    width: 3,
+                    height: 30,
+                    color: Colors.teal.shade500,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "010-2000-0526",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Baloo2",
+                        fontSize: 20),
+                  )
+                ],
+              ),
             )
           ],
         )),
